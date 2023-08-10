3e91f796b5d69b9a4848b1b3b35ea1cff8ad2d1f
Asaf <asafko@mta.ac.il>
2023-08-10 17:06:32 +0300

    Added a login manager

---
+++ FacebookWinFormsApp/LoginManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace BasicFacebookFeatures
+{
+    class LoginManager
+    {
+        private const string k_AppId = "607328698057381";
+        private User m_LoggedInUser;
+        private FacebookWrapper.LoginResult m_LoginResult;
+
+        public User LoggedInUser
+        {
+            get
+            {
+                return m_LoggedInUser;
+            }
+        }
+
+        public FacebookWrapper.LoginResult LoginResult { get; set; }
+
+        public bool Login()
+        {
+            bool loginRes = false;
+            m_LoginResult = FacebookService.Login(
+              k_AppId,
+                /// requested permissions:
+                "email",
+               "public_profile",
+               "user_birthday",
+               "user_events",
+               "user_gender",
+               "user_hometown",
+               "user_friends",
+               "user_posts",
+               "user_photos",
+               "user_likes"
+               );
+
+            if (m_LoginResult.AccessToken == null)
+            {
+                m_LoginResult = null;
+            }
+            else
+            {
+                loginRes = true;
+                m_LoggedInUser = m_LoginResult.LoggedInUser;
+            }
+
+            return loginRes;
+        }
+    }
+}
